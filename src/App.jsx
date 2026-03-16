8db7dcd7b4a0f97e9abc1d62820c64f58f5cc68f
Christina Ortiz <ortizchristina881@gmail.com>
2026-03-16 14:59:51 -0400

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,62 @@
-
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import './App.css'
+import { BrowserRouter,Router,Route } from 'react-router-dom'
+import { Login } from './Pages/Login'
+import { Task } from './Pages/Task'
+import { TaskDetail } from './Pages/TaskDetail'
+import { Error } from './Pages/Error'
+
+
+
+
 
 function App() {
+  
+  
   
 
   return (
     <>
-    </>
-  )
-}
+    <BrowserRouter>
+    <Route>
+      <Route path='/'element={<Login/>}>
+      <Route path='/'Task element={<Task/>}/>
+      <Route path='/' dashboard element={<Login/>}>
+      <Route path='/Task/:id' element={<TaskDetail/>}/>
+      </Route>
+    </Route>
+    </BrowserRouter>
+    
 
-export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    </>
+  
+
+
+
+
+
+
+
+
+
+export default defineConfig({
+	plugins: [tailwindcss()],
+});
+
---
+++ src/Pages/TaskDetail.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TaskDetail = () => {
+  return (
+    <div>TaskDetail</div>
+  )
+}
